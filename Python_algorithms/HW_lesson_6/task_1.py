6d923a59a9ee8260ae21c71049f5050e3c40eb40
Ester <greedolife@yandex.ru>
2021-03-09 12:57:50 +0300

    homework №5

--- Python_algorithms/HW_lesson_6/task_1.py
+++ Python_algorithms/HW_lesson_6/task_1.py
@@ -16,8 +16,63 @@
 С одновременным замером времени (timeit.default_timer())!
 """
 
+from random import randint
+from memory_profiler import memory_usage
+from timeit import default_timer
 
 
+def decoration(func):
+    def wrapper(*args, **kwargs):
+        start = default_timer()
+        memory = memory_usage()
+        result = func(*args, **kwargs)
+        print(f'Использовано памяти: {memory_usage()[0] - memory[0]}')
+        print(f'Использовано времени: {default_timer() - start}')
+        return result
+
+    return wrapper
 
 
+# Первый пример: сравнение генератора и списка
+@decoration
+def my_func():
+    my_list = (i for i in range(999999))
+    yield my_list
 
+
+@decoration
+def my_func2():
+    my_list = [i for i in range(999999)]
+    return my_list
+
+
+my_func()
+my_func2()
+
+'''
+Здесь мы сравниваем работу генератора и создание списка. Генератор тратит меньше памяти, т.к. 
+не хранит целый список, а выдаёт элементы по запросам.
+'''
+
+
+# Второй пример: сравнение работы списка, и встроенной функции map
+@decoration
+def my_func3():
+    my_list = [i for i in range(9999999)]
+    new_list = [i ** 2 for i in my_list]
+    return new_list
+
+
+@decoration
+def my_func4():
+    my_list = [i for i in range(9999999)]
+    new_list = map(lambda i: i ** 2, my_list)
+    return new_list
+
+
+'''
+Как мы видим из результатов, функция map использует гораздо меньше памяти, и экономит время более чем в несколько раз.
+'''
+
+my_func3()
+my_func4()
--- Python_algorithms/HW_lesson_6/task_3.py
+++ Python_algorithms/HW_lesson_6/task_3.py
@@ -5,3 +5,21 @@
 Придумать как это решить!
 Есть очень простое решение!
 """
+
+from memory_profiler import profile
+
+
+@profile
+def my_func(number):
+    def count(number):
+        if number <= 1:
+            return number
+        return count(number - 1) + count(number - 2)
+    return print(count(number))
+
+
+'''
+Если производить замеры обычным методом, мы получаем таблицу на каждый вызов функции.
+Но если обернуть рекурсивную функцию в обычную,
+то как мы видим декоратор выполняется только на обычную функцию, не вызываясь рекурсивно.
+'''
